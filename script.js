70a625bbc98db93267bb62524a0d65cf8a2577d2
Glitch (glitch-hello-website) <none>
2021-05-03 04:37:13 +0000

    🐂👚 Checkpoint ./index.html:31031725/988 ./script.js:31031725/1946

--- script.js
+++ script.js
@@ -128,17 +128,55 @@ var ids = nodes.getIds();
 console.log("ids", ids);
 
 
-function agregarElementos(){ 
-var lista=document.getElementById("MostrarMatriz"); 
-arrayFinal.forEach(function(data,index){
-var linew= document.createElement("li");    
-var contenido = document.createTextNode(data);
-lista.appendChild(linew);
-linew.appendChild(contenido);
 
-})
+
+
+
+function genera_tabla() {
+  // Obtener la referencia del elemento body
+  var body = document.getElementsByTagName("body")[0];
+
+  // Crea un elemento <table> y un elemento <tbody>
+  var tabla   = document.createElement("table");
+  var tblBody = document.createElement("tbody");
+
+  // Crea las celdas
+  for (var i = 0; i < arrayaux.length; i++) {
+    // Crea las hileras de la tabla
+    var hilera = document.createElement("tr");
+
+    for (var j = 0; j < arrayaux.length; j++) {
+      // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+      // texto sea el contenido de <td>, ubica el elemento <td> al final
+      // de la hilera de la tabla
+      var celda = document.createElement("td");
+      var textoCelda = document.createTextNode(arrayaux[i][j]);
+      celda.appendChild(textoCelda);
+      hilera.appendChild(celda);
+    }
+
+    // agrega la hilera al final de la tabla (al final del elemento tblbody)
+    tblBody.appendChild(hilera);
+  }
+
+  // posiciona el <tbody> debajo del elemento <table>
+  tabla.appendChild(tblBody);
+  // appends <table> into <body>
+  body.appendChild(tabla);
+  // modifica el atributo "border" de la tabla y lo fija a "2";
+  tabla.setAttribute("border", "2");
 }
-agregarElementos();
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 añadirnodo();  
