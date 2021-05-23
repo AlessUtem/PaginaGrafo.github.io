81344eb807d9bbf51a08c77d85f646ab70d71626
Glitch (glitch-hello-website) <none>
2021-05-23 23:17:03 +0000

    🐷🌴 Checkpoint ./index.html:31489338/1855 ./script.js:31489143/2858

--- script.js
+++ script.js
@@ -753,61 +753,47 @@ function recargar3(contenido) {
   document.getElementById("hamiltoniano").innerHTML = contenido;
 }
 
-/*
-primsMST() {
-   // Initialize graph that'll contain the MST
-   const MST = new Graph();
-   if (this.nodes.length === 0) {
-      return MST;
-   }
-
-
-   // Select first node as starting node
-   let s = this.nodes[0];
-
-
-   // Create a Priority Queue and explored set
-   let edgeQueue = new PriorityQueue(this.nodes.length * this.nodes.length);
-   let explored = new Set();
-   explored.add(s);
-   MST.addNode(s);
-
-
-   // Add all edges from this starting node to the PQ taking weights as priority
-   this.edges[s].forEach(edge => {
-      edgeQueue.enqueue([s, edge.node], edge.weight);
-   });
-
-
-   // Take the smallest edge and add that to the new graph
-   let currentMinEdge = edgeQueue.dequeue();
-   while (!edgeQueue.isEmpty()) {
-
-
-      // COntinue removing edges till we get an edge with an unexplored node
-      while (!edgeQueue.isEmpty() && explored.has(currentMinEdge.data[1])) {
-         currentMinEdge = edgeQueue.dequeue();
-      }
-      let nextNode = currentMinEdge.data[1];
-
-
-      // Check again as queue might get empty without giving back unexplored element
-      if (!explored.has(nextNode)) {
-         MST.addNode(nextNode);
-         MST.addEdge(currentMinEdge.data[0], nextNode, currentMinEdge.priority);
-         // Again add all edges to the PQ
-         this.edges[nextNode].forEach(edge => {
-            edgeQueue.enqueue([nextNode, edge.node], edge.weight);
-         });
-
-
-         // Mark this node as explored explored.add(nextNode);
-         s = nextNode;
-      }
-   }
-   return MST;
+function prim(nodes=[]){
+    let n = nodes.length;
+    let longitudesAristas = Array.from({length:n}, () =>
+        Array.from({length:n}, () => Infinity));
+    for (let node of nodes){
+        if (Array.isArray(node.parent)){
+            for (let link of node.parent){
+                if (typeof link==="object" && link.hasOwnProperty("value")){
+                    longitudesAristas[node.index][link.index] = link.value;
+                    longitudesAristas[link.index][node.index] = link.value;
+                }
+            }
+        }
+    }
+    let masProximo = [];
+    let distanciaMinima = [];
+    for (let i=0; i<n; i++){
+        masProximo[i] = 0;
+        distanciaMinima[i] = longitudesAristas[i][0];
+    }
+    let resultado = [];
+    for (let i=1; i<n; i++){
+        let minimo = Infinity;
+        let k;
+        for (let j=1; j<n; j++){
+            if (distanciaMinima[j]>=0 && distanciaMinima[j]<minimo){
+                minimo = distanciaMinima[j];
+                k = j;
+            }
+        }
+        resultado.push(`${k},${masProximo[k]}`);
+        distanciaMinima[k] = -1;
+        for (let j=1; j<n; j++){
+            if (longitudesAristas[j][k]<distanciaMinima[j]){
+                distanciaMinima[j] = longitudesAristas[j][k];
+                masProximo[j] = k;
+            }
+        }
+    }
+    return resultado;
 }
-*/
 
 //FUNCION PARA AGREGAR SELECT AL FORMULARIO
 window.onload = function agregarSelect() {
