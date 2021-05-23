c1d1fff9f2bfd303ef230b887f68e634a3da9038
Glitch (glitch-hello-website) <none>
2021-05-23 21:21:31 +0000

    💺🐁 Checkpoint ./script.js:31489143/785:31489338/106 ./index.html:31489338/353

--- script.js
+++ script.js
@@ -770,6 +770,34 @@ window.onload = function agregarSelect() {
   }
 };
 
+/*function kruskal(nodes, edges) {
+    var mst = [];
+    var forest = _.map(nodes, function(node) { return [node]; });
+    var sortedEdges = _.sortBy(edges, function(edge) { return -edge[2]; });
+    while(forest.length > 1) {
+        var edge = sortedEdges.pop();
+        var n1 = edge[0],
+            n2 = edge[1];
+
+        var t1 = _.filter(forest, function(tree) {
+            return _.include(tree, n1);
+        });
+            
+        var t2 = _.filter(forest, function(tree) {
+            return _.include(tree, n2);
+        });
+
+        if (t1 != t2) {
+            forest = _.without(forest, t1[0], t2[0]);
+            forest.push(_.union(t1[0], t2[0]));
+            mst.push(edge);
+        }
+    }
+    return mst;
+}
+
+console.log(kruskal(nodes, edges));*/
+
 var options = {
   manipulation: {
     enabled: true,
