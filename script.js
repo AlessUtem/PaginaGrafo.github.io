b2aa101468c675802945cb93be1af7e2857a3821
Glitch (pagina-grafos) <none>
2021-06-05 00:38:24 +0000

    🚐😻 Checkpoint ./script.js:31489143/4

--- script.js
+++ script.js
@@ -1027,7 +1027,7 @@ function recargar3(contenido) {
   document.getElementById("hamiltoniano").innerHTML = contenido;
 }
 
-/*function prim(nodes=[]){
+function prim(nodes=[]){
     let n = nodes.length;
     let longitudesAristas = Array.from({length:n}, () =>
         Array.from({length:n}, () => Infinity));
@@ -1068,7 +1068,7 @@ function recargar3(contenido) {
     }
     return resultado;
 }
-*/
+
 /*function findMinEdge(edges) {
     let min = null;
     for (const edge of edges) {
